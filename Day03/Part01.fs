f4e956ba84b3a73a465647c8f618dcf75407d15a
Paik Paustian <paik.paustian@gmail.com>
2025-12-03 08:58:34 +0100

    Shorten by a line

--- Day03/Part01.fs
+++ Day03/Part01.fs
@@ -4,5 +4,4 @@ let solution =
     System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input01.txt")
     |> Seq.map (Seq.toArray >> Array.map (_.ToString() >> int))
     |> Seq.map (Seq.fold (fun (l, r) v -> if l >= r then (l, max r v) else (r, v)) (0, 0))
-    |> Seq.map (fun (l, r) -> int (l.ToString() + r.ToString()))
-    |> Seq.sum
+    |> Seq.fold (fun acc v -> acc + (int <| v.ToString())) 0
